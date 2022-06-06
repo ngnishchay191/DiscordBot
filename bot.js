e41d802ad75d3a854705d5256693e972fe0e3976
Nishchay Gupta <ngnishchay191@gmail.com>
2022-06-07 00:00:40 +0530

    Generate Welcome Image
    Added function to send image welcoming the user and tuned the reply to hi, now instead of replying to the message, bot sends a separate message saying hi and @ing the user

--- bot.js
+++ bot.js
@@ -3,11 +3,14 @@ require("dotenv").config()
 
 // const TOKEN = "discord bot token used in env now";
 
+const generateImage = require("./generateImage");
+
 // discord api calls discord servers guilds for some reason
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,8 +20,21 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
     if(message.content == "hi" || message.content == "hello" || message.content == "hey") {
-        message.reply("Hello World");
+        // message.reply("Hello <@${message.author.id}>");
+        let authorId = message.author.id;
+
+        let mentionString = '<@!'+authorId+'>';
+        message.channel.send(mentionString+' pong');
     }
+})
+
+const welcomeChannelID = "983427622874808371";
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member);
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    });
 })
 
 client.login(process.env.TOKEN_BOT)
